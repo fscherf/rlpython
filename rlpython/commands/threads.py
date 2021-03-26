a42fdb5146e3cba1c07c9ecdc4c87785b75257bf
Florian Scherf <f.scherf@pengutronix.de>
2021-03-26 08:38:28 +0100

    commands: threads: add thread-ident argument; add tab completion
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- rlpython/commands/threads.py
+++ rlpython/commands/threads.py
@@ -1,18 +1,67 @@
 import threading
+import traceback
 import sys
 
 from rlpython.utils.argument_parser import ReplArgumentParser
 from rlpython.utils.table import write_table
+from rlpython.utils.strings import color
 
 
 class ThreadsCommand:
     NAME = 'threads'
 
+    def complete(self, text, state, line_buffer):
+        names = []
+
+        for thread in threading.enumerate():
+            names.append(str(thread.getName()))
+
+        candidates = []
+
+        for name in sorted(names):
+            if name.startswith(text):
+                candidates.append(name)
+
+        candidates.append(None)
+
+        return candidates[state]
+
+    def find_frame(self, identifier):
+        for thread in threading.enumerate():
+            if(str(thread.ident) == identifier or
+               thread.getName() == identifier):
+
+                return thread, sys._current_frames()[thread.ident]
+
+        return None, None
+
     def run(self, repl, argv):
         # parse command line
         argument_parser = ReplArgumentParser(repl=repl, prog='threads')
+        argument_parser.add_argument('identifier', nargs='?')
 
-        argument_parser.parse_args(argv[1:])
+        arguments = vars(argument_parser.parse_args(argv[1:]))
+
+        # show stack of given thread
+        if arguments['identifier']:
+            thread, frame = self.find_frame(arguments['identifier'])
+
+            if not thread:
+                repl.write(color('ERROR: invalid thread ident\n', fg='red'))
+
+                return 1
+
+            repl.write('STACK: {} id={}\n'.format(
+                thread.getName(),
+                str(thread.ident),
+            ))
+
+            for line in traceback.format_stack(frame):
+                repl.write(line)
+
+            repl.write('END STACK\n')
+
+            return 0
 
         # write table
         rows = [['Thread ID', 'Thread Name', 'Alive', 'Daemon', 'Task']]
