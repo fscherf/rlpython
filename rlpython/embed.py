fscherf/rlpython

commit 128ff5bc9278314f3f44e53773a1dfc4f4229ca6

repl: fix locals and globals issues
Previously variables defined in the REPL were not accessible to functions or
methods, because rlpython saved them into locals().

    >>> a = 10
    >>> def foo():
    ...     print(a)
    ...
    >>> a
    10
    >>> foo()
    Traceback (most recent call last):
      File "/home/fls/opt/python3.9/lib/python3.9/site-packages/rlpython/runtimes/python_runtime.py", line 211, in run
        return_value = run_code(code, self.globals, self.locals)
      File "<input>", line 1, in <module>
      File "<input>", line 2, in foo
    NameError: name 'a' is not defined
    >>>

The default Python REPL saves variables into globals() which avoids this kind
of problems altogether.

This patch merges rlpythons locals() and globals() to fix this issue and behave
more like the standard Python REPL.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

rlpython/embed.py
@@ -17,12 +17,11 @@ def embed(single_threaded=False, bind='', permissions='600',
         os.environ['RLPYTHON_DEBUG'] = 'True'
 
     # use namespace of caller instead of own if nothing is set
-    if 'globals' not in repl_kwargs and 'locals' not in repl_kwargs:
+    if 'globals' not in repl_kwargs:
         stack = inspect.stack()
         frame_info = stack[1]
 
         repl_kwargs['globals'] = frame_info.frame.f_globals
-        repl_kwargs['locals'] = frame_info.frame.f_locals
 
     # setup warnings
     if 'warnings' not in repl_kwargs:

rlpython/aliases.py
@@ -26,9 +26,7 @@ class Aliases:
 
         _command = command.strip()
 
-        if(_command in self.repl.globals or
-           _command in self.repl.locals):
-
+        if _command in self.repl.globals:
             return command
 
         # resolve aliases

rlpython/command_line.py
@@ -79,7 +79,6 @@ def handle_command_line():
     try:
         embed(
             globals={},
-            locals={},
             single_threaded=True,
             started_from_cmd_line=True,
             **repl_kwargs,

rlpython/completion.py
@@ -20,9 +20,6 @@ class Namespace(dict):
         )
 
     def __getitem__(self, name):
-        if name in self.locals:
-            return self.locals[name]
-
         if name in self.globals:
             return self.globals[name]
 
@@ -34,10 +31,7 @@ class Completer:
         self.repl = repl
 
         self.rlcompleter = rlCompleter(
-            namespace=Namespace(
-                locals=self.repl.locals,
-                globals=self.repl.globals,
-            ),
+            namespace=self.repl.globals,
         )
 
         self._module_cache = []

rlpython/repl.py
@@ -71,7 +71,7 @@ class Repl:
                  prompt_ps2=DEFAULT_PROMPT_PS2,
                  history_file=DEFAULT_HISTORY_FILE,
                  history_size=DEFAULT_HISTORY_SIZE,
-                 globals={}, locals={}, commands=[], **variables):
+                 globals={}, commands=[], **variables):
 
         self.banner = banner
         self.warnings = warnings
@@ -81,7 +81,6 @@ class Repl:
         self.history_file = os.path.expanduser(history_file)
         self.history_size = history_size
         self.globals = globals
-        self.locals = locals
 
         self.exit_code = 0
 
@@ -108,7 +107,6 @@ class Repl:
         self.python_runtime = PythonRuntime(
             repl=self,
             globals=globals,
-            locals=locals,
         )
 
         self.command_runtime = CommandRuntime(

rlpython/runtimes/python_runtime.py
@@ -6,27 +6,29 @@ from rlpython.utils.attribute_table import write_attribute_table
 
 
 class PythonRuntime:
-    def __init__(self, repl, globals={}, locals={}):
+    def __init__(self, repl, globals={}):
         self.repl = repl
         self.globals = globals
-        self.locals = locals
 
         # override print
         self.globals['print'] = self.print_function
         self.globals['_print'] = builtins.print
 
         # prepare special keywords
-        self.locals['_'] = None
-        self.locals['_rlpython'] = self.repl
-        self.locals['_exception'] = None
+        self.globals['_'] = None
+        self.globals['_rlpython'] = self.repl
+        self.globals['_exception'] = None
 
     def shutdown(self):
+
+        # restore default print
         del self.globals['print']
         del self.globals['_print']
 
-        del self.locals['_']
-        del self.locals['_rlpython']
-        del self.locals['_exception']
+        # remove special keywords
+        del self.globals['_']
+        del self.globals['_rlpython']
+        del self.globals['_exception']
 
     def print_function(self, *strings, end='\n'):
         string = ' '.join([str(i) for i in strings]) + end
@@ -165,7 +167,7 @@ class PythonRuntime:
     # code running ############################################################
     def eval(self, source, safe=True):
         try:
-            return eval(source, self.globals, self.locals)
+            return eval(source, self.globals)
 
         except Exception:
             if not safe:
@@ -201,14 +203,14 @@ class PythonRuntime:
                 )
 
             except SyntaxError as exception:
-                self.locals['_exception'] = exception
+                self.globals['_exception'] = exception
                 self.repl.write_exception(exception)
 
                 return 1
 
         # run code
         try:
-            return_value = run_code(code, self.globals, self.locals)
+            return_value = run_code(code, self.globals)
 
         except KeyboardInterrupt:
             exit_code = 1
@@ -218,12 +220,12 @@ class PythonRuntime:
         except Exception as exception:
             exit_code = 1
 
-            self.locals['_exception'] = exception
+            self.globals['_exception'] = exception
             self.repl.write_exception(exception)
 
         else:
             if run_code is eval:
-                self.locals['_'] = return_value
+                self.globals['_'] = return_value
 
                 if describe:
                     self.write_description(return_value, describe)

commit 92b72cb4a2e5dd1ab7d5361730d71513cb105a55

embed: fix locals issue
128ff5bc9278 ("repl: fix locals and globals issues") introduced a bug that
resulted in locals being unreachable to the REPL, when using rlpython.embed().

This patch fixes this issue by merging global- and local variables into one
namespace and setting it as globals in the REPL.

Signed-off-by: Florian Scherf <mail@florianscherf.de>

rlpython/embed.py
@@ -21,7 +21,10 @@ def embed(single_threaded=False, bind='', permissions='600',
         stack = inspect.stack()
         frame_info = stack[1]
 
-        repl_kwargs['globals'] = frame_info.frame.f_globals
+        repl_kwargs['globals'] = {
+            **frame_info.frame.f_globals,
+            **frame_info.frame.f_locals,
+        }
 
     # setup warnings
     if 'warnings' not in repl_kwargs: